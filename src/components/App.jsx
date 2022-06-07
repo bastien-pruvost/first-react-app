c50f547725a9bae343704a5d6d4bfc678d00be41
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-07 15:04:54 +0200

    Add Footer with event onClick and onBlur

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import Banner from './Banner';
 import logo from '../assets/logo.png';
-// import Cart from './Cart'
+import Cart from './Cart';
 import ShoppingList from './ShoppingList';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
         <img src={logo} alt='La maison jungle' className='lmj-logo' />
         <h1 className='lmj-title'>La maison jungle</h1>
       </Banner>
-      {/* <Cart /> */}
+      <Cart />
       <ShoppingList />
+      <Footer />
     </div>
   );
 }
