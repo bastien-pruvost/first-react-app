a283ad99080504374430ef5fe7197e6ca482b6ba
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-07 16:42:13 +0200

    Add open and close button on Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,18 +1,36 @@
+import { useState } from 'react';
 import '../styles/Cart.css';
 
 const Cart = () => {
   const monsteraPrice = 8;
-  const ivyPrice = 10;
-  const flowerPrice = 15;
-  return (
+  const [cart, updateCart] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return isOpen ? (
     <div className='lmj-cart'>
+      <button
+        className='lmj-cart-toggle-button'
+        onClick={() => setIsOpen(false)}
+      >
+        Fermer
+      </button>
       <h2>Panier</h2>
       <ul>
-        <li>Monstera : {monsteraPrice}€</li>
-        <li>Lierre : {ivyPrice}€</li>
-        <li>Fleurs : {flowerPrice}€</li>
+        <li>
+          <h3>Monstera : {monsteraPrice}€</h3>
+          <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+        </li>
       </ul>
-      Total : {monsteraPrice + ivyPrice + flowerPrice}€
+      <h3>Total : {monsteraPrice * cart}€</h3>
+    </div>
+  ) : (
+    <div className='lmj-cart-closed'>
+      <button
+        className='lmj-cart-toggle-button'
+        onClick={() => setIsOpen(true)}
+      >
+        Ouvrir le panier
+      </button>
     </div>
   );
 };
